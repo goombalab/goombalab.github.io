d55b99588ee8d3c13e01100d79312beb07e93ddc
Kevin Li <57026513+kevinli573@users.noreply.github.com>
2024-06-03 11:26:55 -0400

    Fix to light theme only

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -218,17 +218,18 @@ let determineThemeSetting = () => {
 // Determine the computed theme, which can be "dark" or "light". If the theme setting is
 // "system", the computed theme is determined based on the user's system preference.
 let determineComputedTheme = () => {
-  let themeSetting = determineThemeSetting();
-  if (themeSetting == "system") {
-    const userPref = window.matchMedia;
-    if (userPref && userPref("(prefers-color-scheme: dark)").matches) {
-      return "dark";
-    } else {
-      return "light";
-    }
-  } else {
-    return themeSetting;
-  }
+  return "light";
+  // let themeSetting = determineThemeSetting();
+  // if (themeSetting == "system") {
+  //   const userPref = window.matchMedia;
+  //   if (userPref && userPref("(prefers-color-scheme: dark)").matches) {
+  //     return "dark";
+  //   } else {
+  //     return "light";
+  //   }
+  // } else {
+  //   return themeSetting;
+  // }
 };
 
 let initTheme = () => {
